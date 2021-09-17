9beb3c3f323d7f8a18b8e4cabb54030b8f5a20e7
niranjan-kakade <niranjankakade06@gmail.com>
2021-09-17 22:15:28 +0530

    feat:[main]: IntroToOpenAIGym.py now accepts argument of environment name

--- IntroToOpenAIGym.py
+++ IntroToOpenAIGym.py
@@ -1,4 +1,6 @@
+import sys
 import gym
+import logging
 
 
 class RawAgent:
@@ -15,17 +17,24 @@ class RawAgent:
 
 
 class Environment:
-    def __init__(self):
-        self.env = gym.make('CartPole-v0')
+    def __init__(self, env_name):
+        self.env = gym.make(env_name)
         self.state = self.env.reset()
         self.action_space = self.env.action_space
 
 
-environment = Environment()
-agent = RawAgent(environment.state, environment.action_space)
-for _ in range(300):
-    action = agent.get_action()
-    agent.current_state, reward, done, info = environment.env.step(action)
-    environment.env.render()
-environment.env.reset()
-environment.env.close()
+if __name__ == "__main__":
+    try:
+        environment = Environment(sys.argv[1])
+        agent = RawAgent(environment.state, environment.action_space)
+        for _ in range(300):
+            environment.env.render()
+            action = agent.get_action()
+            agent.current_state, reward, done, info = environment.env.step(action)
+            if done:
+                environment.env.reset()
+        environment.env.close()
+    except KeyboardInterrupt:
+        logging.warning('Keyboard Interrupt')
+    except Exception as e:
+        logging.error('Error at LINE --> {} --> {}'.format(sys.exc_info()[2].tb_lineno, e))
